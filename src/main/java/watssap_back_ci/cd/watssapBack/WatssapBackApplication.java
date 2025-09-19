1a60b14f4ed1f9850408c0739ee52972e40c0047
Rokhayatoure <rokhayatoure2000@gmail.com>
2025-09-19 18:26:49 +0000

    first commit

---
+++ src/main/java/watssap_back_ci/cd/watssapBack/WatssapBackApplication.java
@@ -0,0 +1,13 @@
+package watssap_back_ci.cd.watssapBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatssapBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatssapBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/watssap_back_ci/cd/watssapBack/WatssapBackApplicationTests.java
@@ -0,0 +1,13 @@
+package watssap_back_ci.cd.watssapBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatssapBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
